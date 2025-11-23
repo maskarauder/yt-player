ffb31d9545dc567cfc04cdb255210af69000fc75
bobjrsenior <git@bobjrsenior.pw>
2025-11-23 09:37:11 -0500

    Switch to nodriver from undetected-chromedriver
    It's listed as the successor of undetected-chromedriver. The github url
    for it is here: https://github.com/ultrafunkamsterdam/nodriver

--- main.py
+++ main.py
@@ -4,47 +4,45 @@ import os
 import sys
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 
+import asyncio
 from time import sleep
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-#from webdriver_manager.chrome import ChromeDriverManager
-import undetected_chromedriver as uc
+import nodriver as uc
 from bs4 import BeautifulSoup as bs
 import random
 
-driver = uc.Chrome(headless=False, use_subprocess=True, user_data_dir=os.path.join(SCRIPTDIR, 'profile'))
+driver = None
 
 
 playlists_location = 'https://www.youtube.com/@BurrPlays1/playlists'
 
 
-def scroll_to_load_content() -> None:
+async def scroll_to_load_content(page: uc.Tab) -> None:
     try:
         while True:
-            bottom_of_list = driver.find_element(By.TAG_NAME, 'ytd-continuation-item-renderer')
-            ActionChains(driver) \
-                .scroll_to_element(bottom_of_list) \
-                .perform()
-            sleep(.5)
-    except NoSuchElementException:
+            bottom_of_list = await page.find('ytd-continuation-item-renderer', timeout=1)
+            if bottom_of_list:
+                await bottom_of_list.scroll_into_view()
+                await asyncio.sleep(.5)
+            else:
+                break
+    except TimeoutError:
         return
 
 
-def allow_setup_and_login() -> None:
-    driver.get('https://www.youtube.com')
-    sleep(1)
+async def allow_setup_and_login() -> None:
+    await driver.get('https://www.youtube.com')
+    await asyncio.sleep(1)
 
     input('Please login to YouTube and/or configure whatever extensions you want on this profile then press enter to continue...')
 
 
-def get_available_playlists() -> list:
-    driver.get(playlists_location)
-    sleep(1)
-    scroll_to_load_content()
+async def get_available_playlists() -> list:
+    page = await driver.get(playlists_location)
+    await asyncio.sleep(1)
+    await scroll_to_load_content(page)
 
-    soup = bs(driver.page_source, 'html.parser')
+    page_source = await page.get_content()
+    soup = bs(page_source, 'html.parser')
     res = soup.find_all('a', attrs={'class': 'yt-lockup-view-model__content-image'})
 
     vids = []
@@ -53,23 +51,26 @@ def get_available_playlists() -> list:
     return vids
 
 
-def get_last_video_in_playlist() -> str:
-    soup = bs(driver.page_source, 'html.parser')
+async def get_last_video_in_playlist(page: uc.Tab) -> str:
+    page_source = await page.get_content()
+    soup = bs(page_source, 'html.parser')
     res = soup.find_all('span', attrs={'id': 'video-title', 'class': 'style-scope ytd-playlist-panel-video-renderer'})
 
     return res[-1].get('title')
 
 
-def get_current_video_title() -> str:
-    soup = bs(driver.page_source, 'html.parser')
+async def get_current_video_title(page: uc.Tab) -> str:
+    page_source = await page.get_content()
+    soup = bs(page_source, 'html.parser')
     title = soup.find('h1', attrs={'class': 'style-scope ytd-watch-metadata'}) \
                 .find('yt-formatted-string')
 
     return title.get('title')
 
 
-def get_video_duration() -> int:
-    soup = bs(driver.page_source, 'html.parser')
+async def get_video_duration(page: uc.Tab) -> int:
+    page_source = await page.get_content()
+    soup = bs(page_source, 'html.parser')
     #end_of_video = soup.select_one('meta[itemprop="duration"][content]')['content']
     end_of_video = soup.find('span', attrs={'class': 'ytp-time-duration'})
     if end_of_video is None:
@@ -88,44 +89,48 @@ def get_video_duration() -> int:
 
     return int(duration)
 
-def fullscreen_video() -> None:
-    fullscreen_button = driver.find_element(By.CLASS_NAME, 'ytp-fullscreen-button')
-    ActionChains(driver) \
-        .move_to_element(fullscreen_button) \
-        .pause(.5) \
-        .click() \
-        .perform()
+async def fullscreen_video(page: uc.Tab) -> None:
+    fullscreen_button = await page.find('.ytp-fullscreen-button', timeout=10)
+    if fullscreen_button:
+        await fullscreen_button.mouse_move()
+        await asyncio.sleep(0.5)
+        await fullscreen_button.click()
 
-def watch_video(link) -> None:
-    driver.get(link)
-    sleep(5)
+async def watch_video(link) -> None:
+    page = await driver.get(link)
+    await asyncio.sleep(5)
 
-    last_video_in_list = get_last_video_in_playlist()
-    current_video = get_current_video_title()
+    last_video_in_list = await get_last_video_in_playlist(page)
+    current_video = await get_current_video_title(page)
 
-    fullscreen_video()
+    await fullscreen_video(page)
     while current_video != last_video_in_list:
-        sleep(10)
-        current_video = get_current_video_title()
+        await asyncio.sleep(10)
+        current_video = await get_current_video_title(page)
 
-    duration = get_video_duration()
+    duration = await get_video_duration(page)
     print(f'Last vid detected! Sleeping for {duration}')
-    sleep(duration)
+    await asyncio.sleep(duration)
 
 
-if __name__ == '__main__':
+async def main():
+    global driver
+    driver = await uc.start(user_data_dir=os.path.join(SCRIPTDIR, 'profile'), headless=False)
 
     if not '--skip-prompt' in sys.argv:
-        allow_setup_and_login()
+        await allow_setup_and_login()
 
     last_selected = None
     currently_selected = None
     while True:
-        playlists = get_available_playlists()
+        playlists = await get_available_playlists()
 
         while currently_selected == last_selected:
             currently_selected = random.choice(playlists)
 
         last_selected = currently_selected
 
-        watch_video(currently_selected)
+        await watch_video(currently_selected)
+
+if __name__ == '__main__':
+    uc.loop().run_until_complete(main())

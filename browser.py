ae97de0b8856239a8eab313aba7cad5e71e111ef
maskarauder <mask@maskarauder.com>
2025-12-29 13:42:16 -0500

    Adds OBS listener to automatically stop/start stream.

---
+++ browser.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import os
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+
+from time import sleep
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+from config import *
+import random
+
+import undetected_chromedriver as uc
+from bs4 import BeautifulSoup as bs
+
+driver = uc.Chrome(headless=False, use_subprocess=True, user_data_dir=os.path.join(SCRIPTDIR, 'profile'))
+
+
+def scroll_to_load_content() -> None:
+    try:
+        while True:
+            bottom_of_list = driver.find_element(By.TAG_NAME, 'ytd-continuation-item-renderer')
+            ActionChains(driver) \
+                .scroll_to_element(bottom_of_list) \
+                .perform()
+            sleep(.5)
+    except NoSuchElementException:
+        return
+
+
+def allow_setup_and_login() -> None:
+    driver.get('https://www.youtube.com')
+    sleep(1)
+
+    input('Please login to YouTube and/or configure whatever extensions you want on this profile then press enter to continue...')
+
+
+def get_available_playlists() -> list:
+    driver.get(PLAYLISTS_LOCATION)
+    sleep(1)
+    scroll_to_load_content()
+
+    soup = bs(driver.page_source, 'html.parser')
+    res = soup.find_all('a', attrs={'class': 'yt-lockup-view-model__content-image'})
+
+    vids = []
+    for i in res:
+        vids.append('https://www.youtube.com' + i.get('href'))
+    return vids
+
+
+def get_last_video_in_playlist() -> str:
+    soup = bs(driver.page_source, 'html.parser')
+    res = soup.find_all('span', attrs={'id': 'video-title', 'class': 'style-scope ytd-playlist-panel-video-renderer'})
+
+    return res[-1].get('title')
+
+
+def get_current_video_title() -> str:
+    soup = bs(driver.page_source, 'html.parser')
+    title = soup.find('h1', attrs={'class': 'style-scope ytd-watch-metadata'}) \
+                .find('yt-formatted-string')
+
+    return title.get('title')
+
+
+def get_video_duration() -> int:
+    soup = bs(driver.page_source, 'html.parser')
+    #end_of_video = soup.select_one('meta[itemprop="duration"][content]')['content']
+    end_of_video = soup.find('span', attrs={'class': 'ytp-time-duration'})
+    if end_of_video is None:
+        raise Exception('Unable to determine when we should go to the next video!')
+    
+    duration = end_of_video.text
+    
+    tokens = duration.split(':')
+    if len(tokens) == 3:
+        h = int(tokens[0]) * 60 * 60
+        m = int(tokens[1]) * 60
+        return int(tokens[2]) + m + h
+    elif len(tokens) == 2:
+        m = int(tokens[0]) * 60
+        return int(tokens[1]) + m
+
+    return int(duration)
+
+def fullscreen_video() -> None:
+    fullscreen_button = driver.find_element(By.CLASS_NAME, 'ytp-fullscreen-button')
+    ActionChains(driver) \
+        .move_to_element(fullscreen_button) \
+        .pause(.5) \
+        .click() \
+        .perform()
+
+def watch_video(link) -> None:
+    driver.get(link)
+    sleep(5)
+
+    last_video_in_list = get_last_video_in_playlist()
+    current_video = get_current_video_title()
+
+    fullscreen_video()
+    while current_video != last_video_in_list:
+        sleep(10)
+        current_video = get_current_video_title()
+
+    duration = get_video_duration()
+    print(f'Last vid detected! Sleeping for {duration}')
+    sleep(duration)
+
+def perform() -> None:
+    if PAUSE_FOR_BROWSER_LOGIN:
+        allow_setup_and_login()
+
+    last_selected = None
+    currently_selected = None
+    while True:
+        playlists = get_available_playlists()
+
+        while currently_selected == last_selected:
+            currently_selected = random.choice(playlists)
+
+        last_selected = currently_selected
+
+        watch_video(currently_selected)
---
+++ obs.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+# OBS API
+import obsws_python as obs
+
+# App Specific
+from config import *
+from time import sleep
+from pprint import pprint
+from threading import Thread
+
+class OBSThread(Thread):
+    uptime = -1
+    pause = False
+    ready_to_die = False
+
+    def on_exit_started(self, _):
+        """OBS is dying."""
+        print("OBS closing!")
+        self.ready_to_die = True
+
+    def on_stream_state_changed(self, data):
+        """Stream has either started or stopped."""
+        if data.output_active == False:
+            self.pause = True
+
+            if self.uptime >= RESTART_INTERVAL or RESTART_ON_UNEXPECTED_END:
+                if self.uptime < RESTART_INTERVAL:
+                    print('Stream down unexpectedly? Attempting to restart it.')
+                else:
+                    print('Stream restarting at regularly scheduled interval.')
+                    self.uptime = 0
+                self.cl.start_stream()
+            return
+        
+        # Stream switched on
+        self.pause = False
+        self.uptime = 0
+                
+    def __init__(self):
+        self.cl = None
+        self.ecl = None
+        Thread.__init__(self)
+
+    def __del__(self):
+        try:
+            if self.ecl:
+                self.ecl.disconnect()
+        except Exception:
+            pass
+
+        try:
+            if self.cl:
+                self.cl.disconnect()
+        except Exception:
+            pass
+
+    def run(self) -> None:
+        while not self.connect_to_obs():
+            print('Failed to connect to OBS. Retrying in 1 sec...')
+            sleep(1)
+            return
+
+        # If stream is already running, grab the current uptime.        
+        stream_status = self.cl.get_stream_status()
+        if not stream_status.output_active:
+            print('Waiting for stream to start...')
+
+        while not stream_status.output_active:
+            stream_status = self.cl.get_stream_status()
+            sleep(1)
+
+        print(f'Stream is up. Restarting stream in {RESTART_INTERVAL} seconds.')
+        self.uptime = int(stream_status.output_duration // 1000)
+
+        # Start event listeners
+        self.ecl.callback.register(
+            [
+                self.on_exit_started,
+                self.on_stream_state_changed,
+            ]
+        )
+
+        while not self.ready_to_die:
+            if self.uptime >= RESTART_INTERVAL:
+                self.cl.stop_stream()
+                # Event Client will handle the restart.
+
+            sleep(5)
+            if not self.pause:
+                stream_status = self.cl.get_stream_status()
+                self.uptime = int(stream_status.output_duration // 1000)
+
+        print('Shutting down. sleepiHYUCK')
+
+
+    def connect_to_obs(self) -> bool:
+        global source
+
+        cl = obs.ReqClient(host=OBS_HOST, port=OBS_PORT, password=OBS_WEBSOCKET_PASSWORD, timeout=3)
+        if cl is None:
+            return False
+
+        ecl = obs.EventClient(host=OBS_HOST, port=OBS_PORT, password=OBS_WEBSOCKET_PASSWORD, timeout=3)
+        if ecl is None:
+            cl.disconnect()
+            return False
+        
+        self.cl = cl
+        self.ecl = ecl
+        return True
